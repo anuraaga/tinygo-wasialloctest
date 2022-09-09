ad4c4bc50c814e68e9de34c77a431317b286eb42
Anuraag Agrawal <anuraaga@gmail.com>
2022-09-09 11:08:25 +0900

    Add old and new

--- wasimalloc_test.go
+++ wasimalloc_test.go
@@ -4,44 +4,65 @@ import (
 	"context"
 	_ "embed"
 	"github.com/stretchr/testify/require"
-	"testing"
-
 	"github.com/tetratelabs/wazero"
 	"github.com/tetratelabs/wazero/imports/wasi_snapshot_preview1"
+	"testing"
 )
 
 //go:embed testdata/main.wasm
 var wasm []byte
 
+//go:embed testdata/main-dev.wasm
+var wasmDev []byte
+
 func TestMalloc(t *testing.T) {
-	ctx := context.Background()
-	r := wazero.NewRuntimeWithConfig(ctx, wazero.NewRuntimeConfig().WithWasmCore2())
-	defer r.Close(ctx)
+	tests := []struct {
+		name string
+		wasm []byte
+	}{
+		{
+			name: "tinygo 0.25.0",
+			wasm: wasm,
+		},
+		{
+			name: "tinygo dev",
+			wasm: wasmDev,
+		},
+	}
 
-	_, err := wasi_snapshot_preview1.Instantiate(ctx, r)
-	require.NoError(t, err)
+	for _, tc := range tests {
+		tt := tc
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			r := wazero.NewRuntimeWithConfig(ctx, wazero.NewRuntimeConfig().WithWasmCore2())
+			defer r.Close(ctx)
 
-	mod, err := r.InstantiateModuleFromBinary(ctx, wasm)
-	require.NoError(t, err)
+			_, err := wasi_snapshot_preview1.Instantiate(ctx, r)
+			require.NoError(t, err)
 
-	getBuf := mod.ExportedFunction("get_buf")
-	releaseBuf := mod.ExportedFunction("release_buf")
-	work := mod.ExportedFunction("work")
+			mod, err := r.InstantiateModuleFromBinary(ctx, tt.wasm)
+			require.NoError(t, err)
 
-	ret, err := getBuf.Call(ctx)
-	require.NoError(t, err)
-	defer releaseBuf.Call(ctx, ret[0])
+			getBuf := mod.ExportedFunction("get_buf")
+			releaseBuf := mod.ExportedFunction("release_buf")
+			work := mod.ExportedFunction("work")
 
-	bufPtr := uint32(ret[0])
-	mod.Memory().Write(ctx, bufPtr, []byte("bear"))
+			ret, err := getBuf.Call(ctx)
+			require.NoError(t, err)
+			defer releaseBuf.Call(ctx, ret[0])
 
-	ret, err = work.Call(ctx, 0)
-	require.NoError(t, err)
+			bufPtr := uint32(ret[0])
+			mod.Memory().Write(ctx, bufPtr, []byte("bear"))
 
-	buf, ok := mod.Memory().Read(ctx, uint32(ret[0]), 9)
-	require.EqualValues(t, "pandabear", buf)
+			ret, err = work.Call(ctx, 0)
+			require.NoError(t, err)
 
-	buf, ok = mod.Memory().Read(ctx, bufPtr, 4)
-	require.True(t, ok)
-	require.EqualValues(t, "bear", buf)
+			buf, ok := mod.Memory().Read(ctx, uint32(ret[0]), 9)
+			require.EqualValues(t, "pandabear", buf)
+
+			buf, ok = mod.Memory().Read(ctx, bufPtr, 4)
+			require.True(t, ok)
+			require.EqualValues(t, "bear", buf)
+		})
+	}
 }
